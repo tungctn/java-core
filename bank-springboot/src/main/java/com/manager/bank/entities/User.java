f26b8eb19b8d04f934dc922ba217acb909f83e1c
minhnd <nguyenminhhghg01@gmail.com>
2025-02-18 23:46:28 +0700

    join prj, test

--- bank-springboot/src/main/java/com/manager/bank/entities/User.java
+++ bank-springboot/src/main/java/com/manager/bank/entities/User.java
@@ -3,11 +3,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 
 @Entity
 public class User extends Base {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @SequenceGenerator(name = "user_seq", sequenceName = "user_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq")
     private int id;
     private String firstName;
     private String lastName;
