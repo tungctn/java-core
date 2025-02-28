9bf50ca3fe1740b5172882d1fb092c921cf166ce
Your Name <your.email@example.com>
2025-02-28 07:22:54 +0000

    test cicd

--- bank-springboot/src/main/java/com/manager/bank/controller/HelloController.java
+++ bank-springboot/src/main/java/com/manager/bank/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class HelloController {
 
-    @GetMapping("/hello")
+    @GetMapping("/")
     public String hello() {
         return "Xin chào từ Bank Application!";
     }
